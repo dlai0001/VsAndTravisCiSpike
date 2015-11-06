47004b0d7268516ba6ef3fad86a2a76ebc74288a
David Lai <sub+github@dlaisoft.com>
2015-11-05 16:45:07 -0800

    remove mstest reference

--- VsAndTravisCiSpike/UnitTest1.cs
+++ VsAndTravisCiSpike/UnitTest1.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿
 namespace VsAndTravisCiSpike
-{
-    [TestClass]
+{    
     public class UnitTest1
-    {
-        [TestMethod]
+    {        
         public void TestMethod1()
         {
         }
